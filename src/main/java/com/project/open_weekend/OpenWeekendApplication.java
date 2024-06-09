823ea1a973f3ebc5d7983ede37925da9d29e8f31
Abiola David <davidolanrewaju2000@gmail.com>
2024-06-09 21:35:20 +0100

    Initialized project

---
+++ src/main/java/com/project/open_weekend/OpenWeekendApplication.java
@@ -0,0 +1,13 @@
+package com.project.open_weekend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWeekendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenWeekendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/open_weekend/OpenWeekendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.open_weekend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeekendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
